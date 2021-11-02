f1ffd7c560f5aea38e6f9f3ce5105ecaf259c933
Ishtiaque1997 <60317006+Ishtiaque1997@users.noreply.github.com>
2021-11-02 07:33:44 +0600

    some done

--- src/Pages/Home/Services/Services.js
+++ src/Pages/Home/Services/Services.js
@@ -1,6 +1,20 @@
 import React from 'react';
+
+https://ibb.co/CHd46fT
+
+
+
+
+
+https://ibb.co/MDLMdfR
 const services=[
- {name:'Replace tyre',price:2000,time:2,img:''}
+ {name:'Replace tyre',price:2000,time:2,img:'https://ibb.co/dPkhZMH'},
+ {name:'Replace tyre',price:2000,time:2,img:'https://ibb.co/B64c2Tc'},
+ {name:'Replace tyre',price:2000,time:2,img:'https://ibb.co/3FS5xpL'},
+ {name:'Replace tyre',price:2000,time:2,img:'https://ibb.co/x2t3tTY'},
+ {name:'Replace tyre',price:2000,time:2,img:'https://ibb.co/8PfBQX3'},
+ {name:'Replace tyre',price:2000,time:2,img:'https://ibb.co/r67x56h'}
+
 ]
 const Services = () => {
  return (
