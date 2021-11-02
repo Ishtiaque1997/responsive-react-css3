99574d7a08ffa90acf2685f991efcd15602c5f91
Ishtiaque1997 <60317006+Ishtiaque1997@users.noreply.github.com>
2021-11-02 07:15:10 +0600

    first-commit

---
+++ src/Pages/Home/Services/Services.js
@@ -0,0 +1,13 @@
+import React from 'react';
+const services=[
+ {name:'Replace tyre',price:2000,time:2,img:''}
+]
+const Services = () => {
+ return (
+  <div>
+   
+  </div>
+ );
+};
+
+export default Services;
