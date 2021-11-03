562d7925f1566e3b798b0421379f0c2708754144
Ishtiaque1997 <60317006+Ishtiaque1997@users.noreply.github.com>
2021-11-03 10:07:25 +0600

    auth-setup-done

--- src/Pages/Login/Login/Login.js
+++ src/Pages/Login/Login/Login.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import useFirebase from '../../../hooks/useFirebase';
 
 const Login = () => {
+ const{signInUsingGoogle,users,logOut}=useFirebase();
  return (
   <div>
    <h4>Please login</h4>
-   <button className="btn btn-warning">Google Sign In</button>
+   <button className="btn btn-warning" onClick={signInUsingGoogle}>Google Sign In</button>
   </div>
  );
 };
---
+++ src/hooks/useFirebase.js
@@ -0,0 +1,38 @@
+import { getAuth, signInWithPopup, 
+ onAuthStateChanged,GoogleAuthProvider ,signOut} from "firebase/auth";
+import { useEffect, useState } from "react";
+import initializeAuthentication from "../Pages/Login/Firebase/firebase.init";
+initializeAuthentication();
+
+const useFirebase=()=>{
+ const[users,setUsers]=useState({});
+ const auth=getAuth();
+ const signInUsingGoogle=()=>{
+   const googleProvider=new GoogleAuthProvider();
+   signInWithPopup(auth,googleProvider)
+   .then(res=>{
+    setUsers(res.user);
+   })
+ }
+ useEffect(()=>{
+  const unsubscribed=onAuthStateChanged(auth,user=>{
+   if(user){
+    setUsers(user)
+   }
+   else{
+    setUsers({})
+   }
+  });
+  return()=>unsubscribed;
+ },[])
+ const logOut=()=>{
+  signOut(auth)
+  .then(()=>{})
+ }
+ return{
+  users,
+  signInUsingGoogle,
+  logOut
+ }
+}
+export default useFirebase;
